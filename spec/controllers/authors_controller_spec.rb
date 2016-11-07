b8209a1274c8e1b48fdcc1d718231a071c3f3658
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-07 13:11:00 +0100

    next try

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
 
-  describe "GET #new" do
+  describe "GET #NewAuthorPage" do
     it "returns http success" do
-      get :new
+      get :NewAuthorPage
       expect(response).to have_http_status(:success)
     end
   end

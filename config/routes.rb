4390202bacbda13a0c12bd870329ce794be745e9
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-07 12:55:59 +0100

    ah shit commit -a wasnt what I wanted or was it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'authors/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   root 'home#index'
  

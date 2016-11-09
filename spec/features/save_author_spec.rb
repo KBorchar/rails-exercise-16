ce5f57bbe78eb49fc8eca045e0b93bed3b74e98f
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-09 19:25:11 +0100

    "fixes that once fixed issue"

--- spec/features/save_author_spec.rb
+++ spec/features/save_author_spec.rb
@@ -14,4 +14,11 @@ describe "New Author Page", :type => :feature do
     Author.find_by_homepage "http://wikipedia.org/Alan_Turing"
 
   end
+
+  it "should have text input for first name, last name, and homepage" do
+    visit new_author_path
+    expect(page).to have_field("First name")
+    expect(page).to have_field("Last name")
+    expect(page).to have_field("Homepage")
+  end
 end

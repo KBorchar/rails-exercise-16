624cbd60afeaa2217570190328d36c1803c373f9
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-10 20:55:25 +0100

    Added failing test for #13

--- spec/features/authors_index_spec.rb
+++ spec/features/authors_index_spec.rb
@@ -8,6 +8,11 @@ describe "Author index page", :type => :feature do
 		expect(page).to have_text("Alan Turing") 
 
 	end
+
+	it "should link to the new author page" do
+		visit authors_path
+		expect(page).to have_css("a", text: "Add Author")
+	end
 end
 
 

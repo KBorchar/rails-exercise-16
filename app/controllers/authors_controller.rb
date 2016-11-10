6995531faf82a13927452de9d8639f2799a93eb2
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-10 18:07:57 +0100

    Author page should render, fixes #7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,12 +2,18 @@ class AuthorsController < ApplicationController
   def new
   end
 
-  def create
-    render plain: params[:article].inspect
-    @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
-    @author.save()
-    #redirect_to @author
+  def index
+    @authors = Author.all
   end
 
+  def create
+    #render plain: params[:author].inspect
+    @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+    @author.save()
+    redirect_to @author
+  end
+  def show
+    @author = Author.find(params[:id])
+  end
 
 end
--- spec/features/show_author_spec.rb
+++ spec/features/show_author_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 describe "Author page", :type => :feature do
   
   it "should render, given an author named 'Alan Turing'" do
-    author = create:author
+    author = create(:author)
 	  visit author_path(author)
       end
 end

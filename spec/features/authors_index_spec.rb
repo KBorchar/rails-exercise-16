d504102fe1c291b7da8a8a202b1b396de72064f7
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-10 20:45:24 +0100

    Index page lists name and homepage of all authors, fixes #12

--- spec/features/authors_index_spec.rb
+++ spec/features/authors_index_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe "Author index page", :type => :feature do
 	it "should render " do
-		visit authors_path
+		
+		create(:author)
+		visit authors_path	
 		expect(page).to have_text("Alan Turing") 
 
 	end

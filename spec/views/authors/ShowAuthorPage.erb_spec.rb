d848e6ce212b34c6bd006e22635571735d4aff7a
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-10 20:02:15 +0100

    fixed test and fixed author page showing homepage fixes #10

--- spec/views/authors/ShowAuthorPage.erb_spec.rb
+++ spec/views/authors/ShowAuthorPage.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 describe "Author page", :type => :feature  do
 
-	it "should show the author's first name" do
+	it "should show the author's first name, last name and homepage" do
 		author = create(:author)
 		visit author_path(author)
 		expect(page).to have_text("Alan")
-	end
-	it "should show the author's last name" do
-		author = create(:author)
-		visit author_path(author)
 		expect(page).to have_text("Turing")
-	end	
+		expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+	end
 end
 

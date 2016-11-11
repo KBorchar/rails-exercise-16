e27afcbf78ec1c493e25bb3931f2019fa17e29b1
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-11 17:27:29 +0100

    generated paper controller and modified routes to accomodate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   resources :authors
+  resources :papers
+
   get 'new_author' => 'authors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

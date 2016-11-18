250eede8ee67fa13480c5e2f5f2e5168d1e49074
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-18 23:05:15 +0100

    Add paper-saving, fixes #26

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,8 +1,28 @@
 class PapersController < ApplicationController
-def new
-end
+  def new
+    @paper = Paper.new
+  end
 
-def create
-end
+  def index
+    @papers = Paper.all
+  end
+
+  def show
+    @paper = Paper.find(params[:id])
+  end
+
+  def create
+    @paper = Paper.new(paper_params)
+
+    @paper.save
+    redirect_to @paper
+  end
+
+  def paper_params
+    params.require(:paper).permit(:title, :venue, :year)
+  end
+
+
+
 
 end

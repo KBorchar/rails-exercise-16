048a96d3062659ef8d0a99d8d34e330f0b447f4e
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-19 00:48:04 +0100

    add failing tests for editing and destroying papers from index

--- spec/features/paper_index_spec.rb
+++ spec/features/paper_index_spec.rb
@@ -14,4 +14,25 @@ describe "Paper index page", :type => :feature do
     visit papers_path
     expect(page).to have_css("a", :text => "Add paper")
   end
+
+  it "should link to edit paper page" do
+    create(:paper)
+    visit papers_path
+    expect(page).to have_css("a", :text => "Edit")
+  end
+
+  it "should link to destroying a paper" do
+    create(:paper)
+    visit papers_path
+    expect(page).to have_css("a", :text => "Destroy")
+  end
+
+  it "should delete a paper whos destroy button got clicked." do
+    create(:paper)
+    visit papers_path
+    click_link 'Destroy'
+
+    expect(Paper.first).to be_nil
+  end
 end
+

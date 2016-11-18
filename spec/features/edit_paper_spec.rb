481ad16779362e71613b53533cd513e08562bafd
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-19 00:35:03 +0100

    Add faiing test for #31

--- spec/features/edit_paper_spec.rb
+++ spec/features/edit_paper_spec.rb
@@ -7,15 +7,14 @@ describe "Edit paper page", :type => :feature do
 
   end
 
-  #it "should save changes" do
-  #  @author = create(:author)
-  #  visit edit_author_path(@author)
-  #  fill_in 'author_first_name', with: "Alan Mathison"
-  #  fill_in 'author_homepage', with: "http://wikipedia.org/Alan_Turing"#
+  it "should save changes" do
+    @paper = create(:paper)
+    visit edit_paper_path(@paper)
+    fill_in 'paper_venue', with: "Mind 49"
 
-#    click_button 'Save Author'
-#    Author.find_by! first_name: "Alan Mathison"
-#    Author.find_by! last_name: "Turing"
-#    Author.find_by! homepage: "http://wikipedia.org/Alan_Turing"
-#  end
+    click_button 'Save Paper'
+    Paper.find_by! venue: "Mind 49"
+    #Author.find_by! last_name: "Turing"
+    #Author.find_by! homepage: "http://wikipedia.org/Alan_Turing"
+  end
 end

623e3161cc7de58320a3500a52f23d8fd2d0b3c5
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-19 11:14:38 +0100

    add failing test for #35

--- spec/features/edit_paper_spec.rb
+++ spec/features/edit_paper_spec.rb
@@ -17,4 +17,11 @@ describe "Edit paper page", :type => :feature do
     #Author.find_by! last_name: "Turing"
     #Author.find_by! homepage: "http://wikipedia.org/Alan_Turing"
   end
+
+  it "should allow to select 5 authors from 5 separate drop downs" do
+    p = create(:paper)
+    visit edit_paper_path(p)
+    expect(page).to have_field("Author 1")
+  end
+
 end

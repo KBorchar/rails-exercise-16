b7b2e8683a9d48bc82705f6d1ea016f55e814198
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-19 13:00:49 +0100

    add saving authors on edit paper, fixes #38

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -46,7 +46,7 @@ class PapersController < ApplicationController
   end
 
   def paper_params
-    params.require(:paper).permit(:title, :venue, :year)
+    params.require(:paper).permit(:title, :venue, :year, { author_ids: []})
   end
 
 
